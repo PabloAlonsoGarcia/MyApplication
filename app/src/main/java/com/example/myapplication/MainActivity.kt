50ab2963bb0ab1dae533954f366ac79808e4bfcc
PabloAlonsoGarcia <pabalo172@alu.centroafuera.es>
2022-11-17 13:31:51 +0100

    Ejercicio 2 Terminado

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     private var onCreate = 0
@@ -11,12 +13,20 @@ class MainActivity : AppCompatActivity() {
     private var onPause=3
     private var onStop=4
     private var onDestroy=5
+    private lateinit var texto: EditText
+    private lateinit var texto2: EditText
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("onCreate $onCreate")
+        texto=findViewById(R.id.etNombre)
+        texto2=findViewById(R.id.etNombre2)
+
     }
+
 
     override fun onStart(){
         super.onStart()
@@ -41,6 +51,26 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         println("onDestroy $onDestroy")
     }
+
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+
+        savedInstanceState.putString("nombre", texto.text.toString())
+        savedInstanceState.putString("apellido", texto2.text.toString())
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        texto.setText(savedInstanceState.getString("nombre"))
+        texto2.setText(savedInstanceState.getString("apellido"))
+
+
+    }
+
+
 
 
 
